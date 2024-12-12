f57d68f573adf37cac7c3e8bdf89034b06dd8c12
Yanysik111 <yanyisik@yandex.ru>
2024-12-12 15:59:59 +0300

    Add files via upload

---
+++ UrbanDjango/task3/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+# Create your views here.
+def main_page(request):
+    headline = 'Главная страница'
+    text1= 'Главная'
+    text2 = 'Магазин'
+    text3 = 'Корзина'
+    context = {'headline': headline,
+               'text1': text1,
+               'text2': text2,
+               'text3': text3,}
+    return render(request,'third_task/sh1.html', context)
+
+def first_page(request):
+    name = 'Игры'
+    games = ['Atomic Heart','Cyberpank 2077','PayDay 2']
+    ret = 'Вернуться обратно'
+    context = {'name': name,
+               'games': games,
+               'ret': ret}
+    return render(request, 'third_task/sh2.html', context)
+
+def second_page(request):
+    text = 'Извините, Ваша корзина пуста'
+    ret = 'Вернуться обратно'
+    context = {'text': text,
+               'ret': ret}
+    return render(request, 'third_task/sh3.html', context)
